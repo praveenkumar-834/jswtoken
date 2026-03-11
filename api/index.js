6a129424bedd575596c7adcf604e5a44a16e4048
praveenkumar-834 <praveenuu34@gmail.com>
2026-03-11 17:43:46 +0530

    dns update

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "../config/db.js";
 import authRoutes from "../routes/authRoutes.js";
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1"]);
 
 dotenv.config();
 
